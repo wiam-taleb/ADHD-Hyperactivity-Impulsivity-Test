48d66fa5c51392760d9c5418a72e2855e185c6b1
wiam-taleb <wtaleb.contactfirm@gmail.com>
2025-11-01 21:47:24 +0300

    Update app.py

--- ADHD-TSET/src/app.py
+++ ADHD-TSET/src/app.py
@@ -9,7 +9,6 @@ def home():
     return render_template('index.html')
 
 if _name_ == '_main_':
-    # Render يقرأ المتغير PORT من البيئة
     import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
@@ -136,4 +135,5 @@ if __name__ == '__main__':
     print("=" * 50)
 
     app.run(debug=True, port=5000)
+
 
